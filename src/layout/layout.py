af5a71ddd01225ccb58209918e0e2de237e20528
Peyton Fang <peyton888@gmail.com>
2022-02-07 09:34:17 +0800

    Adjust padding and spacing

--- src/layout/layout.py
+++ src/layout/layout.py
@@ -28,7 +28,7 @@ class CalculatorLayout(BoxLayout):
         self.textpanel = CPanel.create()
         self.button_create()
         for row in self.btn_rows:
-            layout_row = BoxLayout(orientation='horizontal')
+            layout_row = BoxLayout(orientation='horizontal', padding=2, spacing=4)
             for btn in row:
                 layout_row.add_widget(btn)
             self.layout_rows.append(layout_row)
